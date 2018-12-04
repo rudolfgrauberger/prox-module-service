aabb830e7f23850e5364e9e57cea34885c1f2256
Julian Lengelsen <julian.lengelsen@th-koeln.de>
2018-12-04 11:19:33 +0100

    Initial commit

---
+++ src/main/java/de/thkoeln/projektboerse/moduleservice/ModuleServiceApplication.java
@@ -0,0 +1,12 @@
+package de.thkoeln.projektboerse.moduleservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModuleServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModuleServiceApplication.class, args);
+	}
+}
---
+++ src/test/java/de/thkoeln/projektboerse/moduleservice/ModuleServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package de.thkoeln.projektboerse.moduleservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ModuleServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
