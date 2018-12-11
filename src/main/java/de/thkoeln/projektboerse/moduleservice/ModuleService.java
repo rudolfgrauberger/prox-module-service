cc2a5b431a86e6ab14414a8a020a2a8072db0996
Julian Lengelsen <julian.lengelsen@th-koeln.de>
2018-12-11 11:42:15 +0100

    Rename the Spring Boot entrypoint class

--- src/main/java/de/thkoeln/projektboerse/moduleservice/ModuleService.java
+++ src/main/java/de/thkoeln/projektboerse/moduleservice/ModuleService.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ModuleServiceApplication {
+public class ModuleService {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ModuleServiceApplication.class, args);
+		SpringApplication.run(ModuleService.class, args);
 	}
 }
--- src/test/java/de/thkoeln/projektboerse/moduleservice/ModuleServiceTests.java
+++ src/test/java/de/thkoeln/projektboerse/moduleservice/ModuleServiceTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class ModuleServiceApplicationTests {
+public class ModuleServiceTests {
 
 	@Test
 	public void contextLoads() {
