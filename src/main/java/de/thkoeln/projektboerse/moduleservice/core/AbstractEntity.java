1c5224f5e64ec0a6415c705e6a78a863a9d77c96
Julian Lengelsen <julian.lengelsen@th-koeln.de>
2019-01-21 16:27:55 +0100

    Change AbstractEntity to abstract class

--- src/main/java/de/thkoeln/projektboerse/moduleservice/core/AbstractEntity.java
+++ src/main/java/de/thkoeln/projektboerse/moduleservice/core/AbstractEntity.java
@@ -14,7 +14,7 @@ import lombok.ToString;
 @Getter
 @ToString
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class AbstractEntity {
+public abstract class AbstractEntity {
 
   @Id
   @GeneratedValue
