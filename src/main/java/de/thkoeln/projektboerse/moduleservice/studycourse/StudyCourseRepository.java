c9da09e11b3443409591c3a910a5f3b79a732f0c
Mansoor Rahmati <mansoor_rahmati@web.de>
2019-01-31 17:14:59 +0100

    Allowed CORS request

--- src/main/java/de/thkoeln/projektboerse/moduleservice/studycourse/StudyCourseRepository.java
+++ src/main/java/de/thkoeln/projektboerse/moduleservice/studycourse/StudyCourseRepository.java
@@ -2,7 +2,9 @@ package de.thkoeln.projektboerse.moduleservice.studycourse;
 
 import java.util.UUID;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin
 public interface StudyCourseRepository extends PagingAndSortingRepository<StudyCourse, UUID> {
 
 }
