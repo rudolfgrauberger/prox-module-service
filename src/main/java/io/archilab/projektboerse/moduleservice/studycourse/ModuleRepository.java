d97c87f87580d3d0b12e59692794086ea27d7b96
Julian Lengelsen <julian.lengelsen@th-koeln.de>
2019-02-04 21:10:03 +0100

    Remove redundant CORS configuration

--- src/main/java/io/archilab/projektboerse/moduleservice/studycourse/ModuleRepository.java
+++ src/main/java/io/archilab/projektboerse/moduleservice/studycourse/ModuleRepository.java
@@ -2,9 +2,7 @@ package io.archilab.projektboerse.moduleservice.studycourse;
 
 import java.util.UUID;
 import org.springframework.data.repository.PagingAndSortingRepository;
-import org.springframework.web.bind.annotation.CrossOrigin;
 
-@CrossOrigin
 public interface ModuleRepository extends PagingAndSortingRepository<Module, UUID> {
 
 }
