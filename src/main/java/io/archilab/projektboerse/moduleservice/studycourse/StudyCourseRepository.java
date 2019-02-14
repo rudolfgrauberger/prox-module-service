3a6113985c2711c82a9f8aed59ce4e63ca8d67f2
Mansoor Rahmati <mansoor_rahmati@web.de>
2019-02-14 13:06:04 +0100

    Add findByAcademicDegree method

--- src/main/java/io/archilab/projektboerse/moduleservice/studycourse/StudyCourseRepository.java
+++ src/main/java/io/archilab/projektboerse/moduleservice/studycourse/StudyCourseRepository.java
@@ -1,8 +1,11 @@
 package io.archilab.projektboerse.moduleservice.studycourse;
 
+import java.util.Set;
 import java.util.UUID;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 public interface StudyCourseRepository extends PagingAndSortingRepository<StudyCourse, UUID> {
 
+    Set<StudyCourse> findByAcademicDegree(AcademicDegree academicDegree);
 }
