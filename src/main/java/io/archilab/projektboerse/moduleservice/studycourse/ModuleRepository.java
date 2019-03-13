60e7018cb9921785ea4bb108176bbc67937e6013
Stephan <44501469+stephanhhhh@users.noreply.github.com>
2019-03-13 11:01:28 +0100

    Security  allow all

--- src/main/java/io/archilab/projektboerse/moduleservice/studycourse/ModuleRepository.java
+++ src/main/java/io/archilab/projektboerse/moduleservice/studycourse/ModuleRepository.java
@@ -2,7 +2,9 @@ package io.archilab.projektboerse.moduleservice.studycourse;
 
 import java.util.UUID;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public interface ModuleRepository extends PagingAndSortingRepository<Module, UUID> {
 
 }
