37e65a3e5cd34b7b6ac0fe099ddc183aefdec4df
Stephan <44501469+stephanhhhh@users.noreply.github.com>
2019-03-13 15:09:40 +0100

    filter

--- src/main/java/io/archilab/projektboerse/moduleservice/studycourse/ModuleRepository.java
+++ src/main/java/io/archilab/projektboerse/moduleservice/studycourse/ModuleRepository.java
@@ -1,10 +1,14 @@
 package io.archilab.projektboerse.moduleservice.studycourse;
 
+import java.util.List;
 import java.util.UUID;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 @CrossOrigin(origins = "*", allowedHeaders = "*")
 public interface ModuleRepository extends PagingAndSortingRepository<Module, UUID> {
+	
+	 List<Module> findByName_NameContainingIgnoreCase (@Param("name") String name);
 
 }
