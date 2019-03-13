233ded7c72dbf83f8985410abc1f5830400d6fdb
JannIntveen <JannIntveen@gmail.com>
2019-03-13 15:23:21 +0100

    Added Paging support for search Method

--- src/main/java/io/archilab/projektboerse/moduleservice/studycourse/ModuleRepository.java
+++ src/main/java/io/archilab/projektboerse/moduleservice/studycourse/ModuleRepository.java
@@ -2,6 +2,9 @@ package io.archilab.projektboerse.moduleservice.studycourse;
 
 import java.util.List;
 import java.util.UUID;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -9,6 +12,6 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 @CrossOrigin(origins = "*", allowedHeaders = "*")
 public interface ModuleRepository extends PagingAndSortingRepository<Module, UUID> {
 	
-	 List<Module> findByName_NameContainingIgnoreCase (@Param("name") String name);
+	 Page<Module> findByName_NameContainingIgnoreCase (@Param("name") String name, Pageable pageable);
 
 }
