fe05c1f3bd3990d7c72e57940c4ff87eb43bdfd7
Julian Lengelsen <julian.lengelsen@th-koeln.de>
2019-03-28 17:04:05 +0100

    Disable CORS at service level

--- src/main/java/io/archilab/projektboerse/moduleservice/studycourse/StudyCourseRepository.java
+++ src/main/java/io/archilab/projektboerse/moduleservice/studycourse/StudyCourseRepository.java
@@ -5,7 +5,6 @@ import java.util.UUID;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
-//@CrossOrigin(origins = "*", allowedHeaders = "*")
 public interface StudyCourseRepository extends PagingAndSortingRepository<StudyCourse, UUID> {
 
   List<StudyCourse> findByAcademicDegree(AcademicDegree academicDegree);
