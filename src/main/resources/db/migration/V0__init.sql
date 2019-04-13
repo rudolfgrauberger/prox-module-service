06ae1fa480da15c43a52989497f33922bc8a66a9
Florian Baldus <florian.baldus@t-online.de>
2019-04-13 08:48:59 +0200

    remove migration table from V0__init

--- src/main/resources/db/migration/V0__init.sql
+++ src/main/resources/db/migration/V0__init.sql
@@ -1,15 +1,3 @@
-CREATE TABLE IF NOT EXISTS "module-db".flyway_schema_history (
-                                                 installed_rank integer NOT NULL,
-                                                 version character varying(50),
-                                                 description character varying(200) NOT NULL,
-                                                 type character varying(20) NOT NULL,
-                                                 script character varying(1000) NOT NULL,
-                                                 checksum integer,
-                                                 installed_by character varying(100) NOT NULL,
-                                                 installed_on timestamp without time zone DEFAULT now() NOT NULL,
-                                                 execution_time integer NOT NULL,
-                                                 success boolean NOT NULL
-);
 
 CREATE TABLE IF NOT EXISTS public.hops_module_mapping (
                                           id uuid NOT NULL,
